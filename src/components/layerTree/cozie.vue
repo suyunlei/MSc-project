3113d07aa8443580acbdfd6b0050e42ae3349598
suyunlei <805118737@qq.com>
2023-05-29 16:35:25 +0800

    Cozie Manager Done

--- src/components/layerTree/cozie.vue
+++ src/components/layerTree/cozie.vue
@@ -21,33 +21,51 @@
         </el-tree>
       </div>
 
-      <div class="timeStamp">
-        <p>Time Range</p>
-        <el-date-picker
-          v-model="time_start"
-          type="datetime"
-          placeholder="Select time start"
-          value-format="yyyy-MM-dd HH:mm:ss"
-          :picker-options="{
-            selectableRange: '00:00:00 - 23:59:59',
-          }"
-        >
-        </el-date-picker>
-        <el-date-picker
-          v-model="time_end"
-          type="datetime"
-          placeholder="Select time end"
-          value-format="yyyy-MM-dd HH:mm:ss"
-          :picker-options="{
-            selectableRange: '00:00:00 - 23:59:59',
-          }"
-        >
-        </el-date-picker>
-      </div>
-      <div class="submit_btn">
-        <el-button type="primary" @click="submit">Submit</el-button>
-        <!--还需要增加一个获取最新数值的按钮, 还不知道怎么放置-->
-      </div>
+      <el-tabs :tab-position="tab_position" style="height: 100%" type="card">
+        <el-tab-pane label="TimeStamp">
+          <div class="timeStamp">
+            <!-- <p>Time Range</p> -->
+            <el-date-picker
+              v-model="time_start"
+              type="datetime"
+              placeholder="Select time start"
+              value-format="yyyy-MM-dd HH:mm:ss"
+              :picker-options="{
+                selectableRange: '00:00:00 - 23:59:59',
+              }"
+            >
+            </el-date-picker>
+            <el-date-picker
+              v-model="time_end"
+              type="datetime"
+              placeholder="Select time end"
+              value-format="yyyy-MM-dd HH:mm:ss"
+              :picker-options="{
+                selectableRange: '00:00:00 - 23:59:59',
+              }"
+            >
+            </el-date-picker>
+          </div>
+
+          <div class="submit_btn">
+            <el-button type="primary" @click="submit">Submit</el-button>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="First N Rows">
+          <div class="inputTab">
+            <el-input-number
+              v-model="firstNrows"
+              :min="1"
+              :max="10"
+              label="Number of Rows"
+            >
+            </el-input-number>
+          </div>
+          <div class="submit_btn">
+            <el-button type="primary" @click="submit_first_n">Submit</el-button>
+          </div>
+        </el-tab-pane>
+      </el-tabs>
     </Popup>
   </div>
 </template>
@@ -69,6 +87,8 @@ export default {
       cozieData: [],
       time_start: "",
       time_end: "",
+      tab_position: "left",
+      firstNrows: 1,
     };
   },
   mounted() {
@@ -97,7 +117,9 @@ export default {
       });
     },
 
-    handleCheckChange(data, checked) {},
+    handleCheckChange(data, checked) {
+      console.log(data, checked);
+    },
 
     close() {
       //
@@ -252,6 +274,132 @@ export default {
           });
       });
     },
+
+    /**
+     * @description: submit the first n rows of data
+     */
+    submit_first_n() {
+      let nrows = this.firstNrows;
+      let checkedNodes = this.$refs.cozieTree.getCheckedNodes(); //[{},{}]
+      this.get_first_n_rows(checkedNodes, nrows);
+    },
+
+    /**
+     * @description: Get the first n rows of data for one participant
+     * @param {Array[{},{}]} ids
+     * @param {Number} nrows
+     */
+    get_first_n_rows(ids, nrows) {
+      // loading animation
+      const rLoading = this.openLoading();
+
+      let requestCount = ids.length;
+
+      // Add request interceptor
+      axios.interceptors.request.use(
+        function (config) {
+          // Request starts, increment the request counter
+          requestCount++;
+          return config;
+        },
+        function (error) {
+          return Promise.reject(error);
+        }
+      );
+
+      // Add response interceptor
+      axios.interceptors.response.use(
+        function (response) {
+          // Request ends, decrement the request counter
+          requestCount--;
+          // If the request counter is 0, it means all requests have completed, execute custom code
+          if (requestCount === 0) {
+            // Execute your code here
+            console.log("All axios requests have completed");
+            rLoading.close(); // Close the loading animation
+          }
+          return response;
+        },
+        function (error) {
+          return Promise.reject(error);
+        }
+      );
+
+      const url =
+        "https://c3sclddcgcwy5tvzpfumcuggoa0unbuf.lambda-url.ap-southeast-1.on.aws/";
+
+      ids.forEach((id) => {
+        var data = {
+          api_key: "bqXYG83JNPa2l2uCi2zXZp08xxx",
+          id_experiment: "dev",
+          id_participant: id.label,
+          rows: nrows,
+        };
+
+        axios
+          .post(url, data)
+          .then((res) => {
+            console.log(`Status: ${res.status}`);
+            console.log("Body: ", res.data);
+
+            // res.data might be empty
+            if (!Array.isArray(res.data)) {
+              console.log("res.data is not an array");
+              return;
+            }
+
+            // if it's not empty, add the data to the viewer
+            res.data.forEach((data) => {
+              let lat = data.ws_latitude;
+              let lon = data.ws_longitude;
+              let height = data.ws_altitude;
+              window.viewer.entities.add({
+                name: "Participant",
+                position: Cesium.Cartesian3.fromDegrees(lon, lat, height),
+                point: {
+                  pixelSize: 10,
+                  color: Cesium.Color.RED,
+                  outlineColor: Cesium.Color.WHITE,
+                  outlineWidth: 3,
+                },
+                description: `
+                              <div class="cesium-infoBox-description">
+                                <table class="cesium-infoBox-defaultTable">
+                                  <tbody>
+                                    <tr>
+                                      <th>q_location</th>
+                                      <td>${data.q_location}</td>
+                                    </tr>
+                                    <tr>
+                                      <th>acoustic_condition</th>
+                                      <td>${data.acoustic_condition}</td>
+                                    </tr>
+                                    <tr>
+                                      <th>q_thermal_condition</th>
+                                      <td>${data.q_thermal_condition}</td>
+                                    </tr>
+                                    <tr>
+                                      <th>q_visual_condition</th>
+                                      <td>${data.q_visual_condition}</td>
+                                    </tr>
+                                    <tr>
+                                      <th>q_air_quality_condition</th>
+                                      <td>${data.q_air_quality_condition}</td>
+                                    </tr>
+                                    <tr>
+                                      <th>general_comfort_condition</th>
+                                      <td>${data.q_general_comfort_condition}</td>
+                                    </tr>
+                                    <tr>
+                                `,
+              });
+            });
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      });
+    },
   },
 };
 </script>
@@ -274,6 +422,29 @@ export default {
 
   /deep/ .el-tree-node__content:hover {
     background-color: rgba(245, 247, 250, 0.2);
+  }
+  /deep/ .el-tabs__header {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    margin-left: 30px;
+  }
+
+  /deep/ .el-tabs__item {
+    width: 120px;
+    height: 60px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    color: white;
+  }
+
+  /deep/ .el-tabs__item.is-active {
+    background-color: rgba(245, 247, 250, 0.2);
+    color: rgb(102, 177, 255);
   }
 
   /deep/ .scrollbar {
@@ -342,8 +513,22 @@ export default {
     margin: 0 10px;
     font-size: large;
   }
+  /deep/ .el-input-number {
+    width: 60% !important;
+  }
 
   .timeStamp {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    margin-top: 20px;
+    margin-left: 30px;
+    margin-right: 30px;
+  }
+
+  .inputTab {
     display: flex;
     flex-direction: column;
     justify-content: center;
